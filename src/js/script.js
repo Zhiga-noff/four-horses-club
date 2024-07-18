c89fb5bb2587743139c497f41b49a6270aac8bb4
kirill <kzhiganov@zvezdamedia.ru>
2024-07-18 23:04:25 +0300

    Заверстал десктоп надо настроить анимации

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,8 @@
 import {swiperConfig} from "./configs-libs/swiper.config.js";
 import {marqueeConfig} from "./configs-libs/marquee.config.js";
 import {scrollToElement} from "./utils/scroll-to-element.js";
+import {isWebp} from "./utils/image-is-webp.js";
 
-function isWebp() {
-    function testWebP(callback) {
-        var webP = new Image();
-        webP.onload = webP.onerror = function () {
-            callback(webP.height == 2);
-        };
-        webP.src =
-            'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
-    }
-
-    testWebP(function (support) {
-        if (support == true) {
-            document.querySelector('body').classList.add('webp');
-        } else {
-            document.querySelector('body').classList.add('no-webp');
-        }
-    });
-}
 
 isWebp();
 
