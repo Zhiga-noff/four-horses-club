e67ab6f80c0ada2fc3a770a00186a7b521426e78
KZHiganov <kzhiganov@zvezdamedia.ru>
2024-07-18 17:22:33 +0300

    Возник затык с галпом

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,39 @@
 // Функция для конвертации фотографий в формат webp
+import { Swiper } from 'swiper';
+
 function isWebp() {
-	function testWebP(callback) {
-		var webP = new Image();
-		webP.onload = webP.onerror = function () {
-			callback(webP.height == 2);
-		};
-		webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-	}
-	testWebP(function (support) {
-		if (support == true) {
-			document.querySelector('body').classList.add('webp');
-		} else {
-			document.querySelector('body').classList.add('no-webp');
-		}
-	});
+  function testWebP(callback) {
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+      callback(webP.height == 2);
+    };
+    webP.src =
+      'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+  }
+
+  testWebP(function (support) {
+    if (support == true) {
+      document.querySelector('body').classList.add('webp');
+    } else {
+      document.querySelector('body').classList.add('no-webp');
+    }
+  });
 }
 
 isWebp();
+
+$('.run-line').marquee({
+  //duration in milliseconds of the marquee
+  duration: 50_000,
+  //gap in pixels between the tickers
+  gap: 34,
+  //time in milliseconds before the marquee will start animating
+  delayBeforeStart: 0,
+  //'left' or 'right'
+  direction: 'left',
+  //true or false - should the marquee be duplicated to show an effect of continues flow
+  duplicated: true,
+  startVisible: true,
+});
+
+export const swiper = new Swiper('.swiper', {});
