5bce37d011bb78f9b4bf68e34c03f7bbb00abd98
kirill <kzhiganov@zvezdamedia.ru>
2024-07-19 01:41:17 +0300

    На данный момент запутался как сделать слайдер с самолетом

--- src/js/script.js
+++ src/js/script.js
@@ -9,7 +9,8 @@ isWebp();
 $('.run-line').marquee(marqueeConfig);
 
 // Свайпер слайдер
-const swiper = new Swiper('.swiper', swiperConfig);
+const swiper = new Swiper('.fifth-block__slider', swiperConfig);
+let swiperTable = null
 const swiperBtns = document.querySelector('.fifth-block__buttons')
 
 
@@ -17,6 +18,27 @@ swiperBtns.addEventListener('click', () => {
     swiper.activeIndex + 1
 })
 
+const tableForthBlock = document.querySelector('.fourth-block__table')
+const elementsSliderTable = tableForthBlock.querySelectorAll('.fourth-block__element')
+
+if (window.innerWidth <= 992) {
+    tableForthBlock.classList.add('swiper')
+    elementsSliderTable.forEach(item => {
+        item.classList.add('swiper-slide')
+    })
+    tableForthBlock.innerHTML = ``
+    // console.log(swiperConfig.slidesPerView)
+    // swiperConfig.params.slidesPerView = 1
+    // swiperTable = new Swiper('.fourth-block__table', {})
+}
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth <= 992) {
+        tableForthBlock.classList.add('swiper')
+    } else {
+        tableForthBlock.classList.remove('swiper')
+    }
+})
 
 // Плавный скролл
 const buttonContainerMain = document.querySelector('.main-block__buttons')
